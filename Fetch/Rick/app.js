0393c5ec0d3f18ca4203ab5e88fdab539d15e5d9
ItzMarlon2 <marlonamorteguicampo2@gmail.com>
2021-11-26 10:32:56 -0500

    Práctica fetch API sin front

---
+++ Fetch/Rick/app.js
@@ -0,0 +1,67 @@
+const URL = 'https://rickandmortyapi.com/api/character/'
+
+const main = document.getElementById('main')
+const templateCarrito = document.getElementById('templateCarrito').content
+const select = document.querySelector('.select')
+
+const fragment = document.createDocumentFragment()
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetchApi()
+})
+
+const fetchApi = ()=>{
+    fetch(URL)
+    .then(res => res.json())
+    .then(producto=>{
+        main.innerHTML=""
+        const arreglo = producto.results
+
+        seleccionarOpcion(arreglo)
+    })
+}
+
+const seleccionarOpcion= arreglo=>{
+    arreglo.forEach(data=>{
+        const option= document.createElement('option')
+        option.setAttribute('value', data.name)
+        option.classList.add('option')
+        option.textContent=data.name
+        fragment.appendChild(option)
+    })
+    select.appendChild(fragment)
+    crearCard(arreglo)
+    select.addEventListener('change', function (){
+        crearCard(arreglo)
+    })
+        
+}
+
+
+
+const crearCard=arreglo=>{
+    if(select.value=="todos"){
+        main.innerHTML=""
+            arreglo.forEach(arreglo =>{
+            templateCarrito.querySelector('.titu-card').textContent=arreglo.name
+            templateCarrito.querySelector('.img-card').src=arreglo.image
+            templateCarrito.querySelector('.info-card').textContent=arreglo.species
+            const clone = templateCarrito.cloneNode(true)
+            fragment.appendChild(clone)
+            main.appendChild(fragment)
+        })
+    }else { 
+        main.innerHTML=""
+        const arreglo2 = arreglo.filter(arreglo => arreglo.name === select.value)
+            templateCarrito.querySelector('.titu-card').textContent=arreglo2[0].name
+            templateCarrito.querySelector('.img-card').src=arreglo2[0].image
+            templateCarrito.querySelector('.info-card').textContent=arreglo2[0].species
+            const clone = templateCarrito.cloneNode(true)
+            fragment.appendChild(clone)
+            main.appendChild(fragment)
+    }
+
+}
+
+
+
---
+++ Fetch/Simpson/app.js
@@ -0,0 +1,31 @@
+const URL = 'https://thesimpsonsquoteapi.glitch.me/quotes'
+
+const main = document.getElementById('main')
+const templateCarrito = document.getElementById('templateCarrito').content
+const btn_card = document.querySelector('.btn-generar')
+
+const fragment = document.createDocumentFragment()
+
+const fetchApi = ()=>{
+    fetch(URL)
+    .then(res => res.json())
+    .then(producto=>{
+        main.innerHTML=""
+        crearCard(producto)
+    })
+}
+
+btn_card.addEventListener('click', fetchApi)
+
+
+
+const crearCard = producto =>{
+    templateCarrito.querySelector('.titu-card').textContent=producto[0].character
+    templateCarrito.querySelector('.img-card').src=producto[0].image
+    templateCarrito.querySelector('.info-card').textContent=producto[0].quote
+    const clone = templateCarrito.cloneNode(true)
+    fragment.appendChild(clone)
+    main.appendChild(fragment)
+}
+
+
